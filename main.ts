87463a603b6bf6c6c14148ec0c5f650bba8825ca
tabinda ajaz <159224662+Tabinaajaz@users.noreply.github.com>
2024-03-20 11:07:10 +0500

    assigment-of-object

---
+++ main.ts
@@ -0,0 +1,39 @@
+interface rollnumber {
+    Rollno1:number
+    Rollno2:number
+ }
+type studenttype ={
+     Name:string;
+fatherName:string;
+timing:string;
+subject:string;
+days:string;
+rollnumber:number;
+};
+
+var data1={
+    Name:"dua",
+    fatherName:"zubair",
+    timing:"9pm",
+    subject:"english",
+    days:"monday",
+   rollnumber: { 
+    Rollno1:23414,
+
+
+   }
+};
+console.log(data1);
+
+var data2= {
+    Name: "aisha",
+    fatherName: "malik",
+    timing: "9pm",
+    subject: "english",
+    days: "monday",
+    Rollno2: 45689,
+};
+console.log(data2);
+
+
+
